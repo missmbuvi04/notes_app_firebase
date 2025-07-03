58c450999da9d99e887dd08b514c8d98bb5583f7
missmbuvi04 <m.mbuvi@alustudent.com>
2025-07-03 20:20:36 +0300

    finalising my project

--- lib/data/datasources/note_remote_ds.dart
+++ lib/data/datasources/note_remote_ds.dart
@@ -10,7 +10,7 @@ class NoteRemoteDataSource {
 
   Future<List<NoteModel>> fetchNotes(String uid) async {
     final snap = await _col(uid)
-        .orderBy('createdAt', descending: true) // newest first
+        .orderBy('createdAt', descending: true)
         .get();
     return snap.docs.map(NoteModel.fromDoc).toList();
   }
@@ -26,28 +26,6 @@ class NoteRemoteDataSource {
         'updatedAt': FieldValue.serverTimestamp(),
       });
 
-  Future<void> deleteNote(String uid, String id) => _col(uid).doc(id).delete();
+  Future<void> deleteNote(String uid, String id) =>
+      _col(uid).doc(id).delete();
 }
-
-
-
-
-
-
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import '../models/note_model.dart';
-
-// class NoteRemoteDataSource {
-//   final _db = FirebaseFirestore.instance;
-
-//   CollectionReference _col(String uid) => _db.collection('users').doc(uid).collection('notes');
-
-//   Future<List<NoteModel>> fetchNotes(String uid) async {
-//     final snap = await _col(uid).orderBy('text').get();
-//     return snap.docs.map(NoteModel.fromDoc).toList();
-//   }
-
-//   Future<void> addNote(String uid, String text) => _col(uid).add({'text': text});
-//   Future<void> updateNote(String uid, String id, String text) => _col(uid).doc(id).update({'text': text});
-//   Future<void> deleteNote(String uid, String id) => _col(uid).doc(id).delete();
-// }
--- lib/data/models/note_model.dart
+++ lib/data/models/note_model.dart
@@ -4,8 +4,30 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class NoteModel extends Note {
   NoteModel({required super.id, required super.text});
 
-  factory NoteModel.fromDoc(DocumentSnapshot doc) =>
-      NoteModel(id: doc.id, text: doc['text']);
+  factory NoteModel.fromDoc(DocumentSnapshot<Map<String, dynamic>> doc) =>
+      NoteModel(id: doc.id, text: doc.data()?['text'] ?? '');
 
   Map<String, dynamic> toMap() => {'text': text};
 }
+
+
+
+
+
+
+
+
+
+
+
+// import '../../domain/entities/note.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+
+// class NoteModel extends Note {
+//   NoteModel({required super.id, required super.text});
+
+//   factory NoteModel.fromDoc(DocumentSnapshot doc) =>
+//       NoteModel(id: doc.id, text: doc['text']);
+
+//   Map<String, dynamic> toMap() => {'text': text};
+// }
