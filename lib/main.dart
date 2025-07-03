d7731d66c9983ce5d8c47b8e16a6e12f4afb9cc2
missmbuvi04 <m.mbuvi@alustudent.com>
2025-07-03 18:02:53 +0300

    UI logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'presentation/cubit/auth_cubit.dart';
 import 'presentation/pages/login_page.dart';
 import 'presentation/pages/notes_page.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const NotesApp());
 }
 
@@ -18,15 +17,65 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Notes App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => AuthCubit()),
+      ],
+      child: MaterialApp(
+        title: 'Notes',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: BlocBuilder<AuthCubit, AuthStatus>(
+          builder: (_, state) => switch (state) {
+            AuthStatus.authenticated => const NotesPage(),
+            AuthStatus.unauthenticated => const LoginPage(),
+            AuthStatus.unknown => const Scaffold(body: Center(child: CircularProgressIndicator())),
+          },
+        ),
       ),
-      home: FirebaseAuth.instance.currentUser == null
-          ? const LoginPage()
-          : const NotesPage(),
     );
   }
 }
+
+
+
+
+
+
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'firebase_options.dart';
+// import 'presentation/pages/login_page.dart';
+// import 'presentation/pages/notes_page.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+//   );
+//   runApp(const NotesApp());
+// }
+
+// class NotesApp extends StatelessWidget {
+//   const NotesApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Notes App',
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+//         useMaterial3: true,
+//       ),
+//       home: FirebaseAuth.instance.currentUser == null
+//           ? const LoginPage()
+//           : const NotesPage(),
+//     );
+//   }
+// }
